88b07abe3f92c077793a2e6900c57b66fa34de30
Jonathan LEI <me@xjonathan.dev>
2022-12-21 14:01:57 +0000

    feat: add Firehose logs

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -11,7 +11,7 @@ mod macros;
 
 /// The address of a StarkNet contract.
 #[derive(Copy, Clone, PartialEq, Eq, Hash, Serialize, PartialOrd, Ord)]
-pub struct ContractAddress(StarkHash);
+pub struct ContractAddress(pub StarkHash);
 
 macros::starkhash251::newtype!(ContractAddress);
 macros::starkhash251::deserialization!(ContractAddress);
